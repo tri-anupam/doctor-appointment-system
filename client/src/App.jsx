a400410bec0137994dd2be3cec55003f7a08cff5
Anupam Tripathi <arvindtri.70@gmail.com>
2023-08-21 09:37:19 +0530

    add lazy loading and suspense

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,121 +1,128 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.css";
-import HomePage from "./pages/HomePage";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+//--
 import { useSelector } from "react-redux";
 import Spinner from "./components/Spinner";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
-import ApplyDoctor from "./pages/ApplyDoctor";
-import NotificationPage from "./pages/NotificationPage";
-import Users from "./pages/admin/Users";
-import Doctors from "./pages/admin/Doctors";
-import Profile from "./pages/doctor/Profile";
-import BookingPage from "./pages/BookingPage";
-import Appointments from "./pages/Appointments";
-import DoctorAppointments from "./pages/doctor/DoctorAppointments";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const ApplyDoctor = lazy(() => import("./pages/ApplyDoctor"));
+const NotificationPage = lazy(() => import("./pages/NotificationPage"));
+const Users = lazy(() => import("./pages/admin/Users"));
+const Doctors = lazy(() => import("./pages/admin/Doctors"));
+const Profile = lazy(() => import("./pages/doctor/Profile"));
+const BookingPage = lazy(() => import("./pages/BookingPage"));
+const Appointments = lazy(() => import("./pages/Appointments"));
+const DoctorAppointments = lazy(() =>
+  import("./pages/doctor/DoctorAppointments")
+);
+//--
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
     <>
       <BrowserRouter>
-        {loading ? (
-          <Spinner />
-        ) : (
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <HomePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <PublicRoute>
-                  <Register />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="/apply-doctor"
-              element={
-                <ProtectedRoute>
-                  <ApplyDoctor />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route
-              path="/notification"
-              element={
-                <ProtectedRoute>
-                  <NotificationPage />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route
-              path="/admin/users"
-              element={
-                <ProtectedRoute>
-                  <Users />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route
-              path="/admin/doctors"
-              element={
-                <ProtectedRoute>
-                  <Doctors />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route
-              path="/doctor/profile/:id"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route
-              path="/doctor/book-appointment/:doctorId"
-              element={
-                <ProtectedRoute>
-                  <BookingPage />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route
-              path="/appointments"
-              element={
-                <ProtectedRoute>
-                  <Appointments />
-                </ProtectedRoute>
-              }
-            ></Route>
-            <Route
-              path="/doctor-appointments"
-              element={
-                <ProtectedRoute>
-                  <DoctorAppointments />
-                </ProtectedRoute>
-              }
-            ></Route>
-          </Routes>
-        )}
+        <Suspense fallback={<Spinner />}>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <HomePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute>
+                    <Login />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute>
+                    <Register />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/apply-doctor"
+                element={
+                  <ProtectedRoute>
+                    <ApplyDoctor />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/notification"
+                element={
+                  <ProtectedRoute>
+                    <NotificationPage />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/users"
+                element={
+                  <ProtectedRoute>
+                    <Users />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/doctors"
+                element={
+                  <ProtectedRoute>
+                    <Doctors />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/doctor/profile/:id"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/doctor/book-appointment/:doctorId"
+                element={
+                  <ProtectedRoute>
+                    <BookingPage />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/appointments"
+                element={
+                  <ProtectedRoute>
+                    <Appointments />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/doctor-appointments"
+                element={
+                  <ProtectedRoute>
+                    <DoctorAppointments />
+                  </ProtectedRoute>
+                }
+              ></Route>
+            </Routes>
+          )}
+        </Suspense>
       </BrowserRouter>
     </>
   );
--- client/src/pages/NotificationPage.jsx
+++ client/src/pages/NotificationPage.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { useNavigate } from "react-router-dom";
-import Layout from "./../components/Layout";
+// import Layout from "./../components/Layout";
+const Layout = React.lazy(() => import("./../components/Layout"));
 import { message, Tabs } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { hideLoading, showLoading } from "../redux/features/alertSlice";
 import axios from "axios";
+import Spinner from "../components/Spinner";
 
 const NotificationPage = () => {
   const { user } = useSelector((state) => state.user);
@@ -62,61 +64,66 @@ const NotificationPage = () => {
     }
   };
   return (
-    <Layout>
-      <h1 className="p-3 text-center text-2xl">Notification Page</h1>
-      <Tabs>
-        <Tabs.TabPane tab="Unread" key={0}>
-          <div className="flex justify-end">
-            <h4
-              className="p-2 mr-2 rounded-2xl bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
-              onClick={handleMarkAllRead}
-            >
-              Mark All Read
-            </h4>
-          </div>
-          {user?.notification.map((notificationMsg) => (
-            <div className="">
-              <div
-                className="p-2"
-                onClick={() => navigate(notificationMsg.onClickPath)}
-              >
-                <p className="bg-slate-200 p-1 font-bold rounded">
-                  {notificationMsg.type}
-                </p>
-                <p className="bg-slate-100 p-1 m-1 rounded">
-                  {notificationMsg.message}
-                </p>
+    <div>
+      <Suspense fallback={<Spinner />}>
+        <Layout>
+          <h1 className="p-3 text-center text-2xl">Notification Page</h1>
+          <Tabs>
+            <Tabs.TabPane tab="Unread" key={0}>
+              <div className="flex justify-end">
+                <h4
+                  className="p-2 mr-2 rounded-2xl bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
+                  onClick={handleMarkAllRead}
+                >
+                  Mark All Read
+                </h4>
               </div>
-            </div>
-          ))}
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Read" key={1}>
-          <div className="flex justify-end">
-            <h4
-              className="mr-2 p-2 rounded-2xl bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
-              onClick={handleDeleteAllRead}
-            >
-              Delete All Read
-            </h4>
-          </div>
-          {user?.seennotification.map((notificationMsg) => (
-            <div
-              className="card cursor-pointer"
-              onClick={() => navigate(notificationMsg.onClickPath)}
-            >
-              <div className="p-2">
-                <p className="bg-slate-200 p-1 font-bold rounded">
-                  {notificationMsg.type}
-                </p>
-                <p className="bg-slate-100 p-1 m-1 rounded">
-                  {notificationMsg.message}
-                </p>
+              {user?.notification.map((notificationMsg, id) => (
+                <div key={id}>
+                  <div
+                    className="p-2"
+                    onClick={() => navigate(notificationMsg.onClickPath)}
+                  >
+                    <p className="bg-slate-200 p-1  rounded">
+                      {id + 1}. {"  "}
+                      <b>{notificationMsg.type}</b>
+                    </p>
+                    <p className="bg-slate-100 p-1 m-1 rounded">
+                      {notificationMsg.message}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Read" key={1}>
+              <div className="flex justify-end">
+                <h4
+                  className="mr-2 p-2 rounded-2xl bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
+                  onClick={handleDeleteAllRead}
+                >
+                  Delete All Read
+                </h4>
               </div>
-            </div>
-          ))}
-        </Tabs.TabPane>
-      </Tabs>
-    </Layout>
+              {user?.seennotification.map((notificationMsg, id) => (
+                <div
+                  className="card cursor-pointer"
+                  onClick={() => navigate(notificationMsg.onClickPath)}
+                >
+                  <div className="p-2">
+                    <p className="bg-slate-200 p-1 font-bold rounded">
+                      {id + 1}. {notificationMsg.type}
+                    </p>
+                    <p className="bg-slate-100 p-1 m-1 rounded">
+                      {notificationMsg.message}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </Tabs.TabPane>
+          </Tabs>
+        </Layout>
+      </Suspense>
+    </div>
   );
 };
 
